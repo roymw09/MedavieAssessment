f311e478c3175e2e17e92ef481ac737eeb140068
roywilliams <roymw09@gmail.com>
2022-07-27 11:06:53 -0230

    Added comments to the Fan class

--- src/main/java/Fan.java
+++ src/main/java/Fan.java
@@ -6,6 +6,10 @@ public class Fan {
     private boolean reversed = false;
 
     public void increaseSpeed() {
+        // if the direction is reversed
+        // decrement the speed on each method call while the fan is not at 0
+        // if the increaseSpeed method is called while the fan direction
+        // is reversed and the speed is at 0 then return to speed 3
         if (reversed) {
             if (speed == 0) {
                 speed = 3;
@@ -13,6 +17,9 @@ public class Fan {
             }
             speed--;
         } else {
+            // if the direction is not reversed we increment the fan speed
+            // on each method call until the fan reaches three
+            // then the fan is set to speed 0
             if (speed == 3) {
                 speed = 0;
                 return;
